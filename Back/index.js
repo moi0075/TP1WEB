6319bce4bc247e6fab308d40bc59fab1c01ade20
moi0075 <teoviglietti05@gmail.com>
2025-02-11 14:28:28 +0100

    back6

--- Back/index.js
+++ Back/index.js
@@ -10,10 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 //Servir les fichiers du dossier "front"
-app.use(express.static(path.join(__dirname, '../front'))); 
+app.use(express.static(path.join(__dirname, '../Front'))); 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../front/index.html'));
+    res.sendFile(path.join(__dirname, '../Front/index.html'));
 });
 
 const PORT = 4000;
