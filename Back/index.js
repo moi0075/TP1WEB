7bbcaf7564bc54ee9eee28eae4e0e340acff99ad
moi0075 <teoviglietti05@gmail.com>
2025-02-11 15:21:16 +0100

    bug require

--- Back/index.js
+++ Back/index.js
@@ -1,20 +1,23 @@
-const express = require('express');
-// 
-const cors = require('cors');
-// accépter tt requéte ou juste certaine
-const path = require('path');
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-//créer le serveur
+// Configurer `__dirname` car ES Modules ne l'a pas par défaut
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Créer le serveur
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-//Servir les fichiers du dossier "front"
-app.use(express.static(path.join(__dirname, '../Front'))); 
+// Servir les fichiers du dossier "front"
+app.use(express.static(path.join(__dirname, '../Front')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../Front/index.html'));
 });
 
 const PORT = 4000;
-app.listen(PORT, () => console.log('serveur démarrer sur http://localhost:' + PORT));
+app.listen(PORT, () => console.log('Serveur démarré sur http://localhost:' + PORT));
